be5f6dc5cb6f239c16bcaaf49455fd36fb63f454
jason <jbierbrauer99@gmail.com>
2019-07-23 20:59:55 -0500

    getting started

---
+++ src/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <label>
+      {{ title }}
+    </label>
+     <input type="file" accept="image/*" capture>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Main',
+  data() {
+    return {
+      title: 'This is my fancy title'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
