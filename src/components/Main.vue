62bf6dc1d6834c30d7c759f04cd20481e5abf200
jason <jbierbrauer99@gmail.com>
2020-06-30 21:00:10 -0500

    uploading file, sort of

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -14,6 +14,10 @@
         :width="'340px'"
         />
      </div>
+
+     <form id="uploadbanner" enctype="multipart/form-data" method="post" action="#">
+       <input id="fileupload" type="file" @change="processFile" />
+    </form>
   </div>
 </template>
 
@@ -39,6 +43,20 @@ export default {
     this.pictures.push({url: require('../assets/images/racing_sti.jpg'), name: 'racing_sti.jpg', picId: 1})
     this.pictures.push({url: require('../assets/images/2019_subaru_wrx_sti_angularfront.jpg'), name: '2019_subaru_wrx_sti_angularfront', picId: 2})
     this.pictures.push({url: require('../assets/images/19_WRX_KUO_024_M7Y.png'), name: '19_WRX_KUO_024_M7Y', picId: 3})
+  },
+  methods: {
+    processFile() {
+      console.log('processing file')
+      const reader = new FileReader()
+
+      reader.onload = this.handleFileLoad
+      reader.readAsText(event.target.files[0])
+    },
+    handleFileLoad(event){
+      // console.log(event)
+      document.getElementById('fileupload').textContent = event.target.result
+      console.log(document.getElementById('fileupload').textContent)
+    }
   }
 }
 </script>
